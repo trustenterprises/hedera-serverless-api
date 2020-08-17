cf8102f28369efa64ef439168ed70dcaea4b0ebf
Matt Smithies <info@mattsmithies.co.uk>
2020-08-17 14:44:58 +0100

    Update index.js

--- app/constants/language/index.js
+++ app/constants/language/index.js
@@ -5,5 +5,11 @@ module.exports = {
 			noApikey: 'Please set "x-api-key" in your header',
 			invalidApikey: 'Unable to validate with the supplied "x-api-key"'
 		}
+	},
+
+	// Requests
+	statusRequest: {
+		message: "Your environment status for your client",
+		meta_hint: 'Hide this status endpoint by setting "HIDE_STATUS=TRUE" in your environment'
 	}
 }
