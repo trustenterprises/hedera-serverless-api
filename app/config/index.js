b37fbbd864c9b288d399989e1037ebeb188646df
MattSmithies <info@mattsmithies.co.uk>
2020-08-17 12:02:24 +0100

    Update index.js

--- app/config/index.js
+++ app/config/index.js
@@ -1,8 +1,15 @@
 "use strict"
 
-const { HEDERA_ACCOUNT_ID, HEDERA_PRIVATE_KEY } = process.env
+const {
+	HEDERA_ACCOUNT_ID,
+	HEDERA_PRIVATE_KEY,
+	API_SECRET_KEY,
+	HIDE_STATUS
+} = process.env
 
 export default {
 	accountId: HEDERA_ACCOUNT_ID,
-	privateKey: HEDERA_PRIVATE_KEY
+	privateKey: HEDERA_PRIVATE_KEY,
+	authenticationKey: API_SECRET_KEY,
+	hideStatus: HIDE_STATUS
 }
