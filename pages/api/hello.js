6571c6565ec7b87173fb3afcc1c6dee1dddb0045
MattSmithies <info@mattsmithies.co.uk>
2020-08-17 12:04:54 +0100

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+function HelloHandler(req, res) {
 	res.statusCode = 200
 	res.json({ name: "Matt" })
 }
+
+export default HelloHandler
