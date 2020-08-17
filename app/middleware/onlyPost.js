9cbb77b3d04d85ed7ed86b4cf7044d96f8a69383
Matt Smithies <info@mattsmithies.co.uk>
2020-08-17 18:44:17 +0100

    Create onlyPost.js

---
+++ app/middleware/onlyPost.js
@@ -0,0 +1,17 @@
+import Language from "app/constants/language"
+import Status from "app/constants/status"
+
+const { notAllowed } = Language.middleware.onlyPostResponse
+
+function onlyPost(handler) {
+	return async (req, res) => {
+
+		if (req.method !== "POST") {
+			return res.status(Status.METHOD_NOT_ALLOWED).send(notAllowed(req.method))
+		}
+
+		return handler(req, res)
+	}
+}
+
+export default onlyPost
