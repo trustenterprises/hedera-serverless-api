833b290996d8006f9673b3baa66731c47cc7e4f7
Matt Smithies <info@mattsmithies.co.uk>
2020-08-17 18:44:28 +0100

    Create createTopic.js

---
+++ pages/api/consensus/createTopic.js
@@ -0,0 +1,12 @@
+import onlyPost from "app/middleware/onlyPost"
+import withAuthentication from "app/middleware/withAuthentication"
+import useHashgraphContext from "app/context/useHashgraphContext"
+import prepare from "app/utils/prepare"
+import CreateTopicHandler from "app/handler/createTopicHandler"
+
+// Ensure that this route can only be called with POST
+export default prepare(
+  onlyPost,
+	withAuthentication,
+	useHashgraphContext
+)(CreateTopicHandler)
