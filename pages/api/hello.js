9a1b4fd47f6cf28b944c991ca9f1dcce970c5ffd
Matt Smithies <info@mattsmithies.co.uk>
2020-08-17 18:44:31 +0100

    Delete hello.js

--- pages/api/hello.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-function HelloHandler(req, res) {
-	res.statusCode = 200
-	res.json({ name: "Matt" })
-}
-
-export default HelloHandler
