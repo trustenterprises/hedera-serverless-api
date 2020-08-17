0dcf9762ad7087f784f040bf711e464cb836a6d6
MattSmithies <info@mattsmithies.co.uk>
2020-08-17 12:04:24 +0100

    remove old tests

--- test/unit/environment.test.js
+++
@@ -1,8 +0,0 @@
-import Config from "../../app/config"
-
-const { HEDERA_ACCOUNT_ID, HEDERA_PRIVATE_KEY } = process.env
-
-test("Make sure that the config returns an object of env", () => {
-	expect(Config.accountId).toBe(HEDERA_ACCOUNT_ID)
-	expect(Config.privateKey).toBe(HEDERA_PRIVATE_KEY)
-})
