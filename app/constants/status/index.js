b92a755efe592243ebc34828155735e236283c4f
MattSmithies <info@mattsmithies.co.uk>
2020-08-17 12:08:20 +0100

    Update index.js

--- app/constants/status/index.js
+++ app/constants/status/index.js
@@ -1,15 +1,13 @@
 module.exports = {
+	// Default success
+	OK: 200,
 
-  // Default success
-  OK: 200,
+	// Status used when an invalid authentication key is used
+	UNAUTHORIZED: 401,
 
-  // Status used when an invalid authentication key is used
-  UNAUTHORIZED: 401,
+	// Focused on validation, if a check fails this is used
+	UNPROCESSIBLE_ENTITY: 422,
 
-  // Focused on validation, if a check fails this is used
-  UNPROCESSIBLE_ENTITY: 422,
-
-  // Generic catch-all bad request, unless the server throws a 500
-  BAD_REQUEST: 400,
-
-};
+	// Generic catch-all bad request, unless the server throws a 500
+	BAD_REQUEST: 400
+}
