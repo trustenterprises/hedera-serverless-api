dacc3b93890e8de0e457613b2b16307445986b34
MattSmithies <info@mattsmithies.co.uk>
2020-08-18 00:54:11 +0100

    Create getAccountBalanceHandler.js

---
+++ app/handler/getAccountBalanceHandler.js
@@ -0,0 +1,10 @@
+// The raw request handler before middleware, validators and injected context
+async function GetAccountBalanceHandler (req, res) {
+	const { hashgraphClient } = req.context
+	const response = await hashgraphClient.accountBalanceQuery()
+
+	res.statusCode = 200
+	res.json({ data: response })
+}
+
+export default GetAccountBalanceHandler
