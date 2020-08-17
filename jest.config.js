68da9b58bb2d13def0f96c2559127fcfd7d7aa3a
Matt Smithies <info@mattsmithies.co.uk>
2020-08-17 13:15:03 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -81,7 +81,8 @@ module.exports = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    "^app(.*)$": "<rootDir>/app$1"
+    "^app(.*)$": "<rootDir>/app$1",
+    "^mocks(.*)$": "<rootDir>/__mocks__$1"
   },
 
   //"^Actions(.*)$": "<rootDir>/src/actions$1"
