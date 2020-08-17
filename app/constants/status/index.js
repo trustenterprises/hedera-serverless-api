f34e25f2430b8e972d04f0c08ffcc4adc762d903
MattSmithies <info@mattsmithies.co.uk>
2020-08-17 12:02:27 +0100

    Create index.js

---
+++ app/constants/status/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  // Default success
+  OK: 200,
+
+  // Status used when an invalid authentication key is used
+  UNAUTHORIZED: 401,
+
+  // Focused on validation, if a check fails this is used
+  UNPROCESSIBLE_ENTITY: 422,
+
+  // Generic catch-all bad request, unless the server throws a 500
+  BAD_REQUEST: 400,
+
+};
