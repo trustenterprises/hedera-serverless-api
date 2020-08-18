025f71358c9c77d8040dd79a2e537f3df9b5b689
Matt Smithies <info@mattsmithies.co.uk>
2020-08-18 16:20:36 +0100

    Update createTopicHandler.js

--- app/handler/createTopicHandler.js
+++ app/handler/createTopicHandler.js
@@ -1,7 +1,23 @@
+import Status from "app/constants/status"
+import createTopicRequest from "app/validators/createTopicRequest"
+
 // The raw request handler before middleware, validators and injected context
+// All handlers are going to tightly couple the validation
+
 async function CreateTopicHandler(req, res) {
+	const validationErrors = createTopicRequest(req.body)
+
+	if (validationErrors) {
+		return res
+			.status(Status.UNPROCESSIBLE_ENTITY)
+			.send({ errors: validationErrors })
+	}
+
+	const { memo, enable_private_submit_key } = req.body
+	const topicOptions = { memo, enable_private_submit_key }
+
 	const { hashgraphClient } = req.context
-	const newTopicResponse = await hashgraphClient.createNewTopic()
+	const newTopicResponse = await hashgraphClient.createNewTopic(topicOptions)
 
 	res.statusCode = 200
 	res.json({ data: newTopicResponse })
