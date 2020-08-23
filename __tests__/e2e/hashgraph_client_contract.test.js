7637617b5d72984723bdbb9eef05bbaf660715f5
MattSmithies <info@mattsmithies.co.uk>
2020-08-23 16:03:24 +0100

    Delete hashgraph_client_contract.test.js

--- __tests__/e2e/hashgraph_client_contract.test.js
+++
@@ -1,9 +0,0 @@
-import HashgraphClient from "app/hashgraph/client"
-
-// This test will be removed
-
-test("The HashgraphClient should not throw on creation", () => {
-	expect(() => {
-		new HashgraphClient()
-	}).toThrow();
-})
