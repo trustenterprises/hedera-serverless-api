a82ff5eb4cfd4de044f4c2ffa436c5e60742ce41
MattSmithies <info@mattsmithies.co.uk>
2020-08-24 12:40:25 +0100

    Consensus messaging WIP

--- pages/api/consensus/message.js
+++ pages/api/consensus/message.js
@@ -6,8 +6,9 @@ const {
 	ConsensusTopicCreateTransaction,
 	ConsensusMessageSubmitTransaction
 } = require("@hashgraph/sdk")
+import sleep from "app/utils/sleep"
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+// const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 async function consensusMessageHandler(req, res) {
 	const client = Client.forTestnet()
@@ -22,35 +23,79 @@ async function consensusMessageHandler(req, res) {
 	const transactionReceipt = await transactionId.getReceipt(client)
 	const topicId = transactionReceipt.getConsensusTopicId()
 
+	// We just need
+	// const topicId = "0.0.133351"
+
 	// await sleep(5000)
+	//
+	// const myMirrorClient = new MirrorClient(
+	// 	// "hcs.testnet.mirrornode.hedera.com:5600",
+	// 	// '0.testnet.hedera.com:50211'
+	// )
+	//
+	// const subscription = new MirrorConsensusTopicQuery()
+	// 	.setTopicId(topicId)
+	// 	.setLimit(1)
+	// 	// .setStartTime(16)
+	// 	.subscribe(
+	// 		myMirrorClient,
+	// 		message => {
+	// 			// console.log(message);
+	// 			subscription.unsubscribe()
+	//
+	// 			console.log("received from mirror: ", message.toString()),
+	//
+	// 			res.statusCode = 200
+	// 			res.json({ message })
+	// 		},
+	// 		error => console.log("Error: ", error.toString())
+	// 	)
 
-	const myMirrorClient = new MirrorClient(
-		"hcs.testnet.mirrornode.hedera.com:5600"
-	)
+		const receipt = await (await new ConsensusMessageSubmitTransaction()
+	            .setTopicId(topicId)
+	            .setMessage(`Tetris for jeff`)
+	            .execute(client))
+	            .getReceipt(client);
 
-	new MirrorConsensusTopicQuery()
-		.setTopicId(topicId)
-		.setLimit(1)
-		.subscribe(
-			myMirrorClient,
-			message => {
-				// console.log("received from mirror: ", message.toString()),
+	console.log(receipt);
 
-				res.statusCode = 200
-				res.json({ message })
-			},
-			error => console.log("Error: ", error.toString())
-		)
+	// Confirmation 1 - Executing will run a "precheck"
+   // const transactionId = await new CryptoTransferTransaction()
+   //     .addSender(operatorAccount, 0)
+   //     .addRecipient("0.0.1", 0)
+   //     .setTransactionMemo("testing :)")
+   //     .execute(client);
 
-	for (var i = 0; i < 10; i++) {
-		var hcsMessage = await new ConsensusMessageSubmitTransaction()
-			.setTopicId(topicId)
-			.setMessage("This is data")
-			.execute(client)
-		var hcsMessageReceipt = await hcsMessage.getReceipt(client)
+   // Confirmation 2 - Ask for a receipt
+   // console.log(`attempting to get receipt for transaction id = ${transactionId}\n`);
+   // const receipt = await transactionId.getReceipt(client);
+   // console.log(`transaction ${
+   //     transactionId
+   // } receipt = ${
+   //     JSON.stringify(receipt)
+   // }\n`);
+	 //
+   // // Confirmation 3 - Ask for a record
+   // // Note: these cost HBAR! View the network fees at docs.hedera.com
+   // // You should only request these when it's worth paying for the extra details it provides
+   // console.log(`attempting to get record for transaction id = ${transactionId}\n`);
+   // const record = await transactionId.getRecord(client);
+   // console.log(`transaction ${
+   //     transactionId
+   // } record = ${
+   //     JSON.stringify(record)
+   // }\n`);
 
-		console.log(`Send message ${i}: ${hcsMessageReceipt.toString()}`)
-	}
+	// for (var i = 0; i < 10; i++) {
+	// 	var hcsMessage = await new ConsensusMessageSubmitTransaction()
+	// 		.setTopicId(topicId)
+	// 		// .setMessage({ data: "This is data" })
+	// 		.setMessage("This is data :-)")
+	// 		.execute(client)
+	// 	// var hcsMessageReceipt = await hcsMessage.getReceipt(client)
+	//
+	// 	// console.log(`Send message ${i}: ${hcsMessageReceipt.toString()}`)
+	// }
 }
 
 export default consensusMessageHandler
