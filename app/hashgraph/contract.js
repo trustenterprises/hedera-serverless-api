1efb69517839bbb9eb399d0031be8a08ad3737e3
MattSmithies <info@mattsmithies.co.uk>
2020-08-25 12:19:28 +0100

    Update contract.js

--- app/hashgraph/contract.js
+++ app/hashgraph/contract.js
@@ -4,11 +4,22 @@ function throwUnless(candidate, method) {
 	}
 }
 
+/*
+ * Typescript candidate, if the additional complexity is worth it.
+ */
 class HashgraphClientContract {
 	constructor() {
+		// Handle topic CRUD and other things.
 		throwUnless(this.createNewTopic, "createNewTopic")
 		throwUnless(this.getTopicInfo, "getTopicInfo")
+		// throwUnless(this.updateTopic, "updateTopic")
+		// throwUnless(this.deleteTopic, "deleteTopic")
+
+		// Basic functions for user info.
 		throwUnless(this.accountBalanceQuery, "accountBalanceQuery")
+
+		// Sending a message for capturing consensus timestamps
+		throwUnless(this.sendConsensusMessage, "sendConsensusMessage")
 	}
 }
 
