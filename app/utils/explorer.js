ae11d9509bc18da789ce71bfd16d61c2c6ca1e3f
MattSmithies <info@mattsmithies.co.uk>
2020-12-24 00:28:33 +0000

    lint chore

--- app/utils/explorer.js
+++ app/utils/explorer.js
@@ -2,13 +2,12 @@ import Environment from "app/constants/environment"
 import Config from "app/config"
 
 const ExplorerUrl = {
-	[Environment.TESTNET]: 'https://ledger-testnet.hashlog.io/tx/',
+	[Environment.TESTNET]: "https://ledger-testnet.hashlog.io/tx/",
 	[Environment.PREVIEWNET]: null,
-	[Environment.MAINNET]: 'https://ledger.hashlog.io/tx/',
+	[Environment.MAINNET]: "https://ledger.hashlog.io/tx/"
 }
 
 function getExplorerUrl(tx) {
-
 	const network = Config.HEDERA_NETWORK || Environment.TESTNET
 
 	return {
