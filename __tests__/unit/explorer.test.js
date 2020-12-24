e1d50dcce26f4b34bfc166fc7e61a7cabdd40471
MattSmithies <info@mattsmithies.co.uk>
2020-12-24 00:27:29 +0000

    Tests for explorer url

---
+++ __tests__/unit/explorer.test.js
@@ -0,0 +1,28 @@
+import Explorer from "app/utils/explorer"
+import Config from "app/config"
+
+const placeholderTx = 'test'
+
+test("Ensure that a testnet explorer url returns", () => {
+  Config.HEDERA_NETWORK = 'testnet'
+
+  const url = Explorer.getExplorerUrl(placeholderTx)
+
+	expect(url.explorer_url).toBe(`https://ledger-testnet.hashlog.io/tx/${placeholderTx}`)
+})
+
+test("Ensure that a mainnet explorer url returns", () => {
+  Config.HEDERA_NETWORK = 'mainnet'
+
+  const url = Explorer.getExplorerUrl(placeholderTx)
+
+  expect(url.explorer_url).toBe(`https://ledger.hashlog.io/tx/${placeholderTx}`)
+})
+
+test("Ensure that a previewnet explorer url returns nothing", () => {
+  Config.HEDERA_NETWORK = 'previewnet'
+
+  const url = Explorer.getExplorerUrl(placeholderTx)
+
+	expect(!url.explorer_url)
+})
