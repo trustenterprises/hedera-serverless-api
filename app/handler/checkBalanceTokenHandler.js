489d092e4b6320dc45857e57a671e6f0c1298e41
Matt Smithies <flyinggazelle@hotmail.co.uk>
2021-11-25 17:37:56 +0000

    Create checkBalanceTokenHandler.js

---
+++ app/handler/checkBalanceTokenHandler.js
@@ -0,0 +1,22 @@
+import Response from "app/response"
+
+async function CheckBalanceTokenHandler(req, res) {
+
+	const { id, token_id } = req.query
+
+	const balancePayload = {
+		account_id: id,
+		token_id,
+	}
+
+	const { hashgraphClient } = req.context
+	const balanceResponse = await hashgraphClient.getTokenBalance(balancePayload)
+
+	if (balanceResponse) {
+		return Response.json(res, balanceResponse)
+	}
+
+	return Response.badRequest(res)
+}
+
+export default CheckBalanceTokenHandler
