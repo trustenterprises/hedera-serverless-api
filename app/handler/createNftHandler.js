f66764feda3e3177d338c92324cb7ff6d66a090b
Matt Smithies <flyinggazelle@hotmail.co.uk>
2022-08-02 18:43:37 +0100

    Updated NFT handler with required params and validation

--- app/handler/createNftHandler.js
+++ app/handler/createNftHandler.js
@@ -2,33 +2,33 @@ import createNftRequest from "app/validators/createNftRequest"
 import Response from "app/response"
 
 async function CreateNftHandler(req, res) {
-	// Change to NFT specific
+
 	const validationErrors = createNftRequest(req.body)
 
 	if (validationErrors) {
 		return Response.unprocessibleEntity(res, validationErrors)
 	}
 
-	// TODO:
-	// const {
-	// 	symbol,
-	// 	name,
-	// 	supply,
-	// 	memo,
-	// 	requires_kyc = false,
-	// 	can_freeze = false
-	// } = req.body
-	//
+	const {
+		collection_name,
+		symbol,
+		supply,
+		allow_custom_fees,
+		royalty_fee,
+		fallback_fee,
+		enable_unsafe_keys = false
+	} = req.body
+
 	const { hashgraphClient } = req.context
 
-	// TODO:
 	const token = await hashgraphClient.createNonFungibleToken({
-		// memo,
-		// name,
-		// symbol,
-		// supply,
-		// requires_kyc,
-		// can_freeze
+		collection_name,
+		symbol,
+		supply,
+		allow_custom_fees,
+		royalty_fee,
+		fallback_fee,
+		enable_unsafe_keys
 	})
 
 	Response.json(res, token )
