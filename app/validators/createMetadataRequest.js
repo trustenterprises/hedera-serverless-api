967acfea6f9198e0ba0acd15eadfdd68eb17b891
Matt Smithies <flyinggazelle@hotmail.co.uk>
2022-08-04 06:59:49 +0100

    Strict 412 HIP validation for creating metadata before minting

--- app/validators/createMetadataRequest.js
+++ app/validators/createMetadataRequest.js
@@ -1,25 +1,52 @@
 const Joi = require("@hapi/joi")
 
 /**
- * TODO: WIP
- * 
- * 😬 This is a relatively strict schema of validation for HIP 412
+ * 😬 This is a relatively stricter schema validation for HIP 412
  * to ensure that the data is in the correct format for a given range
- * of scenarios before uploading to IPFS
+ * of scenarios before uploading to IPFS.
+ *
+ * Using this HIP as a point of reference: https://hips.hedera.com/hip/hip-412
+ *
+ * This could be considered "off-spec" as the optional sub-objects have required
+ * properties and don't allow any thing additional.
  */
 const schema = Joi.object().keys({
 	name: Joi.string().required(),
+
 	creator: Joi.string().optional(),
+
 	description: Joi.string().optional(),
+
 	image: Joi.string().optional(),
+
 	format: Joi.string().optional(),
+
 	type: Joi.string().when('image', {
 		is: Joi.string().exist(),
 		then: Joi.required(),
 		otherwise: Joi.forbidden(),
 	}),
-	properties: Joi.object().optional()
 
+	files: Joi.array().items(
+		Joi.object().keys({
+			uri: Joi.string().required(),
+			type: Joi.string().required(),
+			metadata_uri: Joi.string().required(),
+		}).optional()
+	),
+
+	// Properties seems like a free-for-all 😂
+	properties: Joi.object().keys({
+		type: Joi.string().optional(),
+		description: Joi.string().optional(),
+	}).optional().options({ allowUnknown: true }),
+
+	localization: Joi.array().items(
+		Joi.object().keys({
+			uri: Joi.string().required(),
+			locale: Joi.string().required(),
+		}).optional()
+	),
 }).options({ allowUnknown: false })
 
 function createNftRequest(candidate = {}) {
