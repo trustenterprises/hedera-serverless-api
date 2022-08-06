cae7598b575b0cb0ecead0b5aa46db6e0fc03377
Matt Smithies <flyinggazelle@hotmail.co.uk>
2022-08-06 14:32:57 +0100

    Update createMetadataHandler.js

--- app/handler/createMetadataHandler.js
+++ app/handler/createMetadataHandler.js
@@ -1,13 +1,15 @@
 import createMetadataRequest from "app/validators/createMetadataRequest"
 import Response from "app/response"
 import NftStorage from "app/utils/nftStorage"
+import Language from 'app/constants/language'
+
 
 async function CreateMetadataHandler(req, res) {
 
 	const validationErrors = createMetadataRequest(req.body)
 
 	if (validationErrors) {
-		return Response.unprocessibleEntity(res, validationErrors)
+		return Response.unprocessibleEntity(res, validationErrors, Language.ensureNftStorageAvailable.meta)
 	}
 
 	const cid = await NftStorage.storeData(req.body)
