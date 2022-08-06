1352843a5d6fb474ca893b9d53e091833352144c
Matt Smithies <flyinggazelle@hotmail.co.uk>
2022-08-06 14:32:54 +0100

    Create transferNftHandler.js

---
+++ app/handler/transferNftHandler.js
@@ -0,0 +1,29 @@
+import Response from "app/response"
+import transferNftRequest from "app/validators/transferNftRequest"
+
+async function TransferNftHandler(req, res) {
+	const validationErrors = transferNftRequest(req.body)
+
+	if (validationErrors) {
+		return Response.unprocessibleEntity(res, validationErrors)
+	}
+
+	const { receiver_id, token_id, serial_number } = req.body
+
+	const transferPayload = {
+		receiver_id,
+		token_id,
+		serial_number
+	}
+
+	const { hashgraphClient } = req.context
+	const sendResponse = await hashgraphClient.transferNft(transferPayload)
+
+	if (sendResponse) {
+		return Response.json(res, sendResponse)
+	}
+
+	return Response.badRequest(res)
+}
+
+export default TransferNftHandler
