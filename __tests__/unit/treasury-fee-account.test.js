dea2631fff21b051cacf8995723f3a3666b1edc3
Matt Smithies <flyinggazelle@hotmail.co.uk>
2023-02-17 09:53:18 +0000

    Update treasury-fee-account.test.js

--- __tests__/unit/treasury-fee-account.test.js
+++ __tests__/unit/treasury-fee-account.test.js
@@ -4,7 +4,7 @@ import Environment from "app/constants/environment"
 const { MAINNET } = Environment
 
 const ROYALTY_FEE_TREASURY =
-	Config.network === MAINNET ? "0.0.1119570" : "0.0.34319163"
+	Config.network === MAINNET ? "0.0.1119570" : "0.0.1897"
 
 test("Expect that the Trust Enterprises fee account matches expected value", () => {
 	expect(Config.royaltyFeeTreasury).toBe(ROYALTY_FEE_TREASURY)
